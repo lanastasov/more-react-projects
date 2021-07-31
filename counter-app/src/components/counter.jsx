f56608845061148f6e28522cc9dc17553980cfc2
lanastasov <lubomir.anastasov@gmail.com>
2021-07-31 14:02:24 +0300

    03 11 Binding Event Handlers

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,21 +3,38 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
-        tags: ['tag1', 'tag2', 'tag3'],
     };
     
-    renderTags() {
-        if (this.state.tags.length === 0 ) return  <p>There are no tags!</p>
-        return <ul>{ this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+    constructor() {
+        super();
+        this.handleIncrement = this.handleIncrement.bind(this);
+        console.log("Constructor", this);
     }
+
+    handleIncrement() {
+        console.log("Increment Clicked!", this.state.count);
+    }
+    
     render() { 
         return (
             <React.Fragment> 
-                {this.renderTags() }
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm"></button>
             </React.Fragment>
         );
     }
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+    
+    formatCount() {
+        const {count} = this.state;
+        return count === 0 ? 'Zero' : this.state.count;
+    }
 }
- 
+
+
 
 export default Counter;
