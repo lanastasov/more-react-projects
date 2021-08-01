dc6ca03bcc1f5d2aefd9498c0886a8a93a37224b
lanastasov <lubomir.anastasov@gmail.com>
2021-08-01 09:52:03 +0300

    04 14 - Destructuring Arguments

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -1,17 +1,25 @@
-import React, { Component } from 'react'; 
+import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-
-     
-    render() { 
-        return (
-        <div>
-            <button onClick={this.props.onReset} className="btn btn-primary btn-sm m-2">Reset</button>
-            { this.props.counters.map(counter => <Counter key={counter.id} onDelete={this.props.onDelete} onIncrement={this.props.onIncrement} counter={counter} />)}
-        </div> 
-        );
-    }
+  render() {
+    const { onReset, counters, onDelete, onIncrement } = this.props;
+    return (
+      <div>
+        <button onClick={onReset} className="btn btn-primary btn-sm m-2">
+          Reset
+        </button>
+        {counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
+            counter={counter}
+          />
+        ))}
+      </div>
+    );
+  }
 }
- 
+
 export default Counters;
--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const NavBar = (props) => {
+const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
         Navbar{" "}
         <span className="badge badge-pill badge-secondary">
-          {props.totalCounters}
+          {totalCounters}
         </span>
       </a>
     </nav>
