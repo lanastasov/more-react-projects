a5de1f7e75ff3152f2671c7a27df3baf7bbc45b5
lanastasov <lubomir.anastasov@gmail.com>
2021-08-01 09:45:35 +0300

    prittier

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const NavBar = (props) => {
-    return (
-        <nav className="navbar navbar-light bg-light">
-            <a className="navbar-brand" href="#">
-                Navbar <span className="badge badge-pill badge-secondary">{props.totalCounters}</span>
-            </a>
-        </nav>
-      );
-}
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
