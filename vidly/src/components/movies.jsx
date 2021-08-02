a0213cf6730beb52fa3c9f906c16366f24d346ca
lanastasov <lubomir.anastasov@gmail.com>
2021-08-02 15:12:07 +0300

    05 05 - Handling Page Changes

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import Like from "./common/like";
+import Pagination from "./common/pagination";
 import { getMovies } from "../services/fakeMovieService";
 
 class Movies extends Component {
   state = {
     movies: getMovies(),
+    currentPage: 1,
+    pageSize: 4,
   };
 
   handleDelete = (movie) => {
@@ -20,7 +23,14 @@ class Movies extends Component {
     this.setState({ movies });
   };
 
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
+  };
+
   render() {
+    const { length: count } = this.state.movies;
+    const { pageSize, currentPage } = this.state;
+
     if (this.state.movies.length === 0)
       return <p> There are no movies in the database.</p>;
     return (
@@ -62,6 +72,12 @@ class Movies extends Component {
             ))}
           </tbody>
         </table>
+        <Pagination
+          itemsCount={this.state.movies.length}
+          pageSize={pageSize}
+          currentPage={currentPage}
+          onPageChange={this.handlePageChange}
+        />
       </React.Fragment>
     );
   }
