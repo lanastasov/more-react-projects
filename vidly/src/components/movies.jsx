afca4e90008ae267165a6012f89941a2f534bdda
lanastasov <lubomir.anastasov@gmail.com>
2021-12-20 17:19:32 +0200

    05 13 - Filtering - Implement Filtering

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -41,12 +41,20 @@ class Movies extends Component {
 
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage, movies: allMovies } = this.state;
+    const {
+      pageSize,
+      currentPage,
+      selectedGenre,
+      movies: allMovies,
+    } = this.state;
 
     if (this.state.movies.length === 0)
       return <p> There are no movies in the database.</p>;
 
-    const movies = paginate(allMovies, currentPage, pageSize);
+    const filtered = selectedGenre
+      ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
+      : allMovies;
+    const movies = paginate(filtered, currentPage, pageSize);
 
     return (
       <div className="row">
@@ -58,7 +66,7 @@ class Movies extends Component {
           />
         </div>
         <div className="col">
-          <p>Shwoing {this.state.movies.length} movies in the database.</p>
+          <p>Showing {filtered.length} movies in the database.</p>
           <table className="table">
             <thead>
               <tr>
@@ -96,7 +104,7 @@ class Movies extends Component {
             </tbody>
           </table>
           <Pagination
-            itemsCount={this.state.movies.length}
+            itemsCount={filtered.length}
             pageSize={pageSize}
             currentPage={currentPage}
             onPageChange={this.handlePageChange}
