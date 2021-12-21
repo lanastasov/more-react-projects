ebe330565cd257a7d10ac6bb677fcd6e3d937519
lanastasov <lubomir.anastasov@gmail.com>
2021-12-21 12:56:55 +0200

    05 22 - Sorting - Unique Keys - Final

--- vidly/src/components/common/tableBody.jsx
+++ vidly/src/components/common/tableBody.jsx
@@ -5,14 +5,19 @@ class TableBody extends React.Component {
     if (column.content) return column.content(item);
     return _.get(item, column.path);
   };
+  createKey = (item, column) => {
+    return item._id + (column.path || column.key);
+  };
   render() {
     const { data, columns } = this.props;
     return (
       <tbody>
         {data.map((item) => (
-          <tr>
+          <tr key={item._id}>
             {columns.map((column) => (
-              <td>{this.renderCell(item, column)}</td>
+              <td key={this.createKey(item, column)}>
+                {this.renderCell(item, column)}
+              </td>
             ))}
           </tr>
         ))}
