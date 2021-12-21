10734e167d6e89321ed3a05e87b56f660a9bdd10
lanastasov <lubomir.anastasov@gmail.com>
2021-12-21 17:46:22 +0200

    06 10 - Query String Parameter

--- router-app/src/components/posts.jsx
+++ router-app/src/components/posts.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import queryString from "query-string";
 
-const Posts = ({ match }) => {
+const Posts = ({ match, location }) => {
+  const result = queryString.parse(location.search);
+  console.log(result);
   return (
     <div>
       <h1>Posts</h1>
