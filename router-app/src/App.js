3b8402f9f1c38007f93384f7e24d6a5d71fc523d
lanastasov <lubomir.anastasov@gmail.com>
2021-12-21 17:53:21 +0200

    06 11 - Redirects

--- router-app/src/App.js
+++ router-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
@@ -20,7 +20,10 @@ class App extends Component {
             <Route path="/products" component={Products} />
             <Route path="/posts/:year?/:month?" component={Posts} />
             <Route path="/admin" component={Dashboard} />
+            <Redirect from="/messages" to="/posts" />
+            <Route path="/not-found" component={NotFound} />
             <Route path="/" component={Home} />
+            <Redirect to="/not-found" />
           </Switch>
         </div>
       </div>
