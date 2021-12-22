5e619a3cd0defdd5b32d4f5678e34d8096cdd72e
lanastasov <lubomir.anastasov@gmail.com>
2021-12-22 20:20:20 +0200

    07 21 - Exercise 1 - Register Form

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -7,6 +7,7 @@ import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
 import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
 import "./App.css";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <NavBar />
       <main className="container">
         <Switch>
+          <Route path="/register" component={RegisterForm}></Route>
           <Route path="/login" component={LoginForm}></Route>
           <Route path="/movies/:id" component={MovieForm}></Route>
           <Route path="/movies" component={Movies}></Route>
