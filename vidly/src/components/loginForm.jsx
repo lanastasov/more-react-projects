5598c85d50e1b959478b82a4c38c337fcce660ac
lanastasov <lubomir.anastasov@gmail.com>
2021-12-22 12:19:54 +0200

    07 03 - Handling Form Submissions

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 
 class LoginForm extends React.Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Call the server
+    console.log("submitted");
+  };
   render() {
     return (
       <div>
         <h1>Login</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input id="username" type="text" className="form-control" />
