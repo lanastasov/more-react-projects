24244ae16fe3aa8183665bacdfabc084918cd057
lanastasov <lubomir.anastasov@gmail.com>
2021-12-22 13:47:44 +0200

    07 06 - Handling Multiple Inputs

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -18,13 +18,14 @@ class LoginForm extends React.Component {
     console.log("submitted");
   };
 
-  handleChange = (e) => {
+  handleChange = ({ currentTarget: input }) => {
     const account = { ...this.state.account };
-    account.username = e.currentTarget.value;
+    account[input.name] = input.value;
     this.setState({ account });
   };
 
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -32,18 +33,26 @@ class LoginForm extends React.Component {
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input
-              value={this.state.account.username}
+              value={account.username}
               onChange={this.handleChange}
               autoFocus
               ref={this.username}
               id="username"
+              name="username"
               type="text"
               className="form-control"
             />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              name="password"
+              id="password"
+              type="text"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
